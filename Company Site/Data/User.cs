84c4f155b0ae9033c2a7ea29104d7df307512882
Muhammad Asim <asimwattoo6@gmail.com>
2022-08-02 12:54:40 +0500

    Added AdminLayout and AdminNavMenu. Added AdminDashboard.razor file with table, search bar and pagination. Added UserManager and User model.

---
+++ Company Site/Data/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Company_Site.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Designation { get; set; }
+        public string? Department { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string? OfficeLocation { get; set; }
+        public string? Status { get; set; }
+        public string? Access { get; set; }
+    }
+}
---
+++ Company Site/Services/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using Company_Site.Data;
+
+namespace Company_Site.Services
+{
+    public class UserManager
+    {
+
+        /// <summary>
+        /// Returns a list of users from the database
+        /// </summary>
+        /// <returns></returns>
+        public List<User> GetUsers()
+        {
+            //TODO: Get users from the database
+            return new List<User>()
+            {
+                new User(){ Id = 1, FirstName = "Muhammad", LastName="Asim", Designation="CEO", Department="Finance",
+                Email="asimwattoo6@gmail.com",
+                Password = "asimwattoo123",
+                Role="Business Head",
+                DateOfJoining = DateTime.Now.AddDays(-100),
+                DOB=new DateTime(2003, 03, 13),
+                LastLogin = DateTime.Now,
+                OfficeLocation = "Office",
+                Status = "Active",
+                Access = "All"
+                },
+                new User(){ Id = 2, FirstName = "Muhammad", LastName="Asim", Designation="CEO", Department="Finance",
+                Email="asimwattoo6@gmail.com",
+                Password = "asimwattoo123",
+                Role="Business Head",
+                DateOfJoining = DateTime.Now.AddDays(-100),
+                DOB=new DateTime(2003, 03, 13),
+                LastLogin = DateTime.Now,
+                OfficeLocation = "Office",
+                Status = "Active",
+                Access = "All"
+                }
+            };
+        }
+
+    }
+}
