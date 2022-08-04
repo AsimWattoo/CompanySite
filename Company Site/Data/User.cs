62cf9a3486fdf25ad3e5da8fd0abf24935a17733
Muhammad Asim <asimwattoo6@gmail.com>
2022-08-04 15:42:29 +0500

    Added add, edit and delete functionalities to the user page. Used sessions to pass data to the AddUserPage.razor component. Replaced select elements with InputSelect elements and input[type='text'] with InputText element. Added data validation by using DataAnnotaionsValidator and ValidationSummary which uses attributes defined on the properties. Added modal for confirmation of deleting user.

--- Company Site/Data/User.cs
+++ Company Site/Data/User.cs
@@ -1,21 +1,57 @@
-﻿namespace Company_Site.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company_Site.Data
 {
     public class User
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string? FirstName { get; set; }
+
+        [Required]
         public string? LastName { get; set; }
+
+        [Required]
         public string? Designation { get; set; }
+
+        [Required]
         public string? Department { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be minimum 8 characters")]
         public string? Password { get; set; }
+
+        [Required]
         public string? Role { get; set; }
-        public DateTime DateOfJoining { get; set; }
-        public DateTime DOB { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfJoining { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DOB { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime LastLogin { get; set; }
+
+        [Required]
         public string? OfficeLocation { get; set; }
+
+        [Required]
         public string? Status { get; set; }
+
         public string? Access { get; set; }
+
+        [Required]
         public string? Mobile { get; set; }
     }
 }
--- Company Site/Services/UserManager.cs
+++ Company Site/Services/UserManager.cs
@@ -5,6 +5,42 @@ namespace Company_Site.Services
     public class UserManager
     {
 
+        #region Private Members
+
+        /// <summary>
+        /// The static list of users
+        /// TODO: Remove this and replace with database
+        /// </summary>
+        private List<User> Users = new List<User>()
+        {
+            new User(){ Id = 1, FirstName = "Muhammad", LastName="Asim", Designation="CEO", Department="Finance",
+            Email="asimwattoo6@gmail.com",
+            Password = "asimwattoo123",
+            Role="Business Head",
+            DateOfJoining = DateTime.Now.AddDays(-100),
+            DOB=new DateTime(2003, 03, 13),
+            LastLogin = DateTime.Now,
+            OfficeLocation = "Office",
+            Status = "Active",
+            Access = "All",
+            Mobile = "0355968475122"
+            },
+            new User(){ Id = 2, FirstName = "Muhammad", LastName="Asim", Designation="CFO", Department="Finance",
+            Email="asimwattoo@gmail.com",
+            Password = "asimwattoo123",
+            Role="Business Head",
+            DateOfJoining = DateTime.Now.AddDays(-100),
+            DOB=new DateTime(2003, 03, 13),
+            LastLogin = DateTime.Now,
+            OfficeLocation = "Office",
+            Status = "On Vacation",
+            Access = "All",
+            Mobile = "0355968475122"
+            }
+        };
+
+        #endregion
+
         /// <summary>
         /// Returns a list of users from the database
         /// </summary>
@@ -12,33 +48,21 @@ namespace Company_Site.Services
         public List<User> GetUsers()
         {
             //TODO: Get users from the database
-            return new List<User>()
-            {
-                new User(){ Id = 1, FirstName = "Muhammad", LastName="Asim", Designation="CEO", Department="Finance",
-                Email="asimwattoo6@gmail.com",
-                Password = "asimwattoo123",
-                Role="Business Head",
-                DateOfJoining = DateTime.Now.AddDays(-100),
-                DOB=new DateTime(2003, 03, 13),
-                LastLogin = DateTime.Now,
-                OfficeLocation = "Office",
-                Status = "Active",
-                Access = "All",
-                Mobile = "03117995371"
-                },
-                new User(){ Id = 2, FirstName = "Muhammad", LastName="Asim", Designation="CTO", Department="Finance",
-                Email="asimwattoo@gmail.com",
-                Password = "asimwattoo123",
-                Role="Business Head",
-                DateOfJoining = DateTime.Now.AddDays(-100),
-                DOB=new DateTime(2003, 03, 13),
-                LastLogin = DateTime.Now,
-                OfficeLocation = "Office",
-                Status = "On Vacation",
-                Access = "All",
-                Mobile = "03117995371"
-                }
-            };
+            return Users;
+        }
+
+        /// <summary>
+        /// Adds user to the list
+        /// </summary>
+        /// <param name="user"></param>
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+        public void RemoveUser(int id)
+        {
+            Users.Remove(Users.Where(u => u.Id == id).First());
         }
 
     }
