8785effb1727e3d61ed42f5d495e0c52ef8d5909
Muhammad Asim <51132084+AsimWattoo@users.noreply.github.com>
2023-01-10 23:34:38 +0500

    Added Calendar Year and Decade Modes.

---
+++ Company Site/Enum/CalendarMode.cs
@@ -0,0 +1,9 @@
+﻿namespace Company_Site.Enum
+{
+    public enum CalendarMode
+    {
+        Month = 0,
+        Year = 1,
+        Decade = 2,
+    }
+}
--- Company Site/Pages/Components/Calendar.razor.cs
+++ Company Site/Pages/Components/Calendar.razor.cs
@@ -1,7 +1,10 @@
 ﻿using Company_Site.Data;
 using Company_Site.DB;
+using Company_Site.Enum;
+using System.Collections;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Company_Site.Pages.Components
 {
@@ -43,6 +46,25 @@ namespace Company_Site.Pages.Components
         };
 
         /// <summary>
+        /// The list of months
+        /// </summary>
+        private string[] Months = new string[]
+        {
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "June",
+            "July",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        };
+
+        /// <summary>
         /// The days in the month
         /// </summary>
         private List<(string, int)> Days = new List<(string, int)>();
@@ -51,6 +73,26 @@ namespace Company_Site.Pages.Components
         /// The tasks per day for the entire month
         /// </summary>
         private Dictionary<int, List<WorkTask>> _tasks = new Dictionary<int, List<WorkTask>>();
+
+        /// <summary>
+        /// The start of the decade
+        /// </summary>
+        private int DecadeStart = 0;
+
+        /// <summary>
+        /// The end of the decade
+        /// </summary>
+        private int DecadeEnd = 0;
+
+        /// <summary>
+        /// The current value
+        /// </summary>
+        private DateTime Date = DateTime.Now;
+
+        /// <summary>
+        /// The current mode of the calender
+        /// </summary>
+        private CalendarMode Mode = CalendarMode.Year;
 
         #endregion
 
@@ -71,7 +113,10 @@ namespace Company_Site.Pages.Components
         /// </summary>
         protected override void OnInitialized()
         {
-            LoadData();
+            int year = Date.Year % 10;
+            DecadeStart = Date.Year - year;
+            DecadeEnd = DecadeStart + 10;
+            LoadData(Date);
         }
 
         #endregion
@@ -80,8 +125,7 @@ namespace Company_Site.Pages.Components
 
         public void UpdateState()
         {
-            ClearData();
-            LoadData();
+            LoadData(Date);
             StateHasChanged();
         }
 
@@ -92,18 +136,18 @@ namespace Company_Site.Pages.Components
         /// <summary>
         /// Loads data in their respective lists
         /// </summary>
-        private void LoadData()
+        private void LoadData(DateTime date)
         {
-            int month = 1;
-            int numberOfDays = DateTime.DaysInMonth(2023, month);
+            ClearData();
+            int numberOfDays = DateTime.DaysInMonth(date.Year, date.Month);
             for (int i = 1; i <= numberOfDays; i++)
             {
-                DateTime date = new DateTime(2023, month, i);
-                Days.Add((date.ToString("ddd"), i));
+                DateTime d = new DateTime(date.Year, date.Month, i);
+                Days.Add((d.ToString("ddd"), i));
             }
             //Getting tasks for the current month
             List<int> taskForCurrentUser = _dbContext.TaskAssignments.Where(f => f.UserId == UserId).Select(f => f.TaskId).Distinct().ToList();
-            List<WorkTask> tasks = _dbContext.Tasks.Where(f => f.ReminderDate.Month == month && taskForCurrentUser.Contains(f.Id)).ToList();
+            List<WorkTask> tasks = _dbContext.Tasks.Where(f => f.ReminderDate.Month == date.Month && taskForCurrentUser.Contains(f.Id)).ToList();
             //Adding tasks for the days
             foreach (WorkTask task in tasks)
             {
@@ -123,6 +167,77 @@ namespace Company_Site.Pages.Components
         {
             Days.Clear();
             _tasks.Clear();
+        }
+
+        /// <summary>
+        /// Moves the value to the next
+        /// </summary>
+        private void Next()
+        {
+            if(Mode == CalendarMode.Month)
+            {
+                Date = Date.AddMonths(1);
+            }
+            else if(Mode == CalendarMode.Year)
+            {
+                Date = Date.AddYears(1);
+            }
+            else if (Mode == CalendarMode.Decade)
+            {
+                DecadeStart += 10;
+                DecadeEnd += 10;
+            }
+            LoadData(Date);
+        }
+
+        /// <summary>
+        /// Moves the value to the previous value
+        /// </summary>
+        private void Prev()
+        {
+            if (Mode == CalendarMode.Month)
+            {
+                Date = Date.AddMonths(-1);
+            }
+            else if (Mode == CalendarMode.Year)
+            {
+                Date = Date.AddYears(-1);
+            }
+            else if(Mode == CalendarMode.Decade)
+            {
+                DecadeStart -= 10;
+                DecadeEnd -= 10;
+            }
+            LoadData(Date);
+        }
+
+        /// <summary>
+        /// Fires when the mode is changed
+        /// </summary>
+        /// <param name="newMode"></param>
+        private void ModeChanged(CalendarMode newMode)
+        {
+            Mode = newMode;
+        }
+
+        /// <summary>
+        /// Indicates that a month is clicked
+        /// </summary>
+        private void MonthClicked(int month)
+        {
+            Mode = CalendarMode.Month;
+            Date = new DateTime(Date.Year, month + 1, Date.Day);
+            LoadData(Date);
+        }
+
+        /// <summary>
+        /// Indicates that a year is clicked
+        /// </summary>
+        private void YearClicked(int year)
+        {
+            Mode = CalendarMode.Year;
+            Date = new DateTime(year, Date.Month, Date.Day);
+            LoadData(Date);
         }
 
         #endregion
